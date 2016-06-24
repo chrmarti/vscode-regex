9f0b0573d023a3ac192847cd84522df20d74fd7b
Christof Marti <chrmarti@microsoft.com>
2016-06-24 14:54:21 -0700

    RegEx -> Regex

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
     const previewUri = vscode.Uri.parse('regex-preview://authority/regex-preview');
-    const regExHighlight = vscode.window.createTextEditorDecorationType({ backgroundColor: 'rgba(100,100,100,.35)' });
+    const regexHighlight = vscode.window.createTextEditorDecorationType({ backgroundColor: 'rgba(100,100,100,.35)' });
     const matchHighlight = vscode.window.createTextEditorDecorationType({ backgroundColor: 'rgba(255,255,0,.35)' });
     const text = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
@@ -23,16 +23,16 @@ occaecat cupidatat non proident, sunt in culpa qui officia
 deserunt mollit anim id est laborum.
         `.trim();
 
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand('extension.showRegExPreview', showRegExPreview));
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('extension.showRegexPreview', showRegexPreview));
 
-    function showRegExPreview(originEditor: vscode.TextEditor) {
+    function showRegexPreview(originEditor: vscode.TextEditor) {
         const rootPath = vscode.workspace.rootPath;
         if (!rootPath) {
             vscode.window.showWarningMessage('No folder opened yet.');
             return;
         }
 
-        const filePath = path.join(rootPath, '.vscode/RegEx Playground.md');
+        const filePath = path.join(rootPath, '.vscode/Regex Playground.md');
         pathExists(filePath).then(exists => {
 
             const uri = vscode.Uri.parse((exists ? 'file:' : 'untitled:') + encodeURI(filePath));
@@ -46,7 +46,7 @@ deserunt mollit anim id est laborum.
                 }
                 return editor;
             }).then(editor => {
-                new RegExMatchDecorator(editor);
+                new RegexMatchDecorator(editor);
             });
 
         }).then(null, reason => {
@@ -66,11 +66,11 @@ deserunt mollit anim id est laborum.
         });
     }
 
-    interface RegExMatch {
+    interface RegexMatch {
 
         document: vscode.TextDocument;
 
-        regEx: RegExp;
+        regex: RegExp;
 
         range: vscode.Range;
 
@@ -81,16 +81,16 @@ deserunt mollit anim id est laborum.
         range: vscode.Range;
     }
 
-    class RegExMatchDecorator {
+    class RegexMatchDecorator {
 
-        private lastRegExEditor: vscode.TextEditor;
+        private lastRegexEditor: vscode.TextEditor;
         private disposables: vscode.Disposable[] = [];
 
         constructor(private matchEditor: vscode.TextEditor) {
 
             this.disposables.push(vscode.workspace.onDidCloseTextDocument(e => {
-                if (this.lastRegExEditor && e === this.lastRegExEditor.document) {
-                    this.lastRegExEditor = null;
+                if (this.lastRegexEditor && e === this.lastRegexEditor.document) {
+                    this.lastRegexEditor = null;
                     this.matchEditor.setDecorations(matchHighlight, []);
                 } else if (e === matchEditor.document) {
                     this.dispose();
@@ -98,13 +98,13 @@ deserunt mollit anim id est laborum.
             }));
 
             this.disposables.push(vscode.workspace.onDidChangeTextDocument(e => {
-                if ((this.lastRegExEditor && e.document === this.lastRegExEditor.document) || e.document === matchEditor.document) {
+                if ((this.lastRegexEditor && e.document === this.lastRegexEditor.document) || e.document === matchEditor.document) {
                     this.update();
                 }
             }));
 
             this.disposables.push(vscode.window.onDidChangeTextEditorSelection(e => {
-                if (this.lastRegExEditor && e.textEditor === this.lastRegExEditor) {
+                if (this.lastRegexEditor && e.textEditor === this.lastRegexEditor) {
                     this.update();
                 }
             }));
@@ -123,18 +123,18 @@ deserunt mollit anim id est laborum.
         }
 
         private update() {
-            const regExEditor = this.lastRegExEditor = findRegExEditor() || this.lastRegExEditor;
-            const regEx = regExEditor && findRegExAtCaret(regExEditor);
-            const matches = regEx ? findMatches(regEx, this.matchEditor.document) : [];
+            const regexEditor = this.lastRegexEditor = findRegexEditor() || this.lastRegexEditor;
+            const regex = regexEditor && findRegexAtCaret(regexEditor);
+            const matches = regex ? findMatches(regex, this.matchEditor.document) : [];
             this.matchEditor.setDecorations(matchHighlight, matches.map(match => match.range));
 
-            if (regExEditor) {
-                regExEditor.setDecorations(regExHighlight, regExEditor !== vscode.window.activeTextEditor && regEx ? [ regEx.range ] : []);
+            if (regexEditor) {
+                regexEditor.setDecorations(regexHighlight, regexEditor !== vscode.window.activeTextEditor && regex ? [ regex.range ] : []);
             }
         }
     }
 
-    function findRegExEditor() {
+    function findRegexEditor() {
         const activeEditor = vscode.window.activeTextEditor;
         if (!activeEditor || activeEditor.document.languageId !== 'typescript') {
             return null;
@@ -142,26 +142,26 @@ deserunt mollit anim id est laborum.
         return activeEditor;        
     }
 
-    function findRegExAtCaret(editor: vscode.TextEditor): RegExMatch {
+    function findRegexAtCaret(editor: vscode.TextEditor): RegexMatch {
         const anchor = editor.selection.anchor;
         const text = editor.document.lineAt(anchor).text;
-        const regExRegEx = /\/((?:\\\/|\[[^\]]*\]|[^/])+)\/([gimuy]*)/g;
+        const regexRegex = /\/((?:\\\/|\[[^\]]*\]|[^/])+)\/([gimuy]*)/g;
 
         let match: RegExpExecArray;
-        while ((match = regExRegEx.exec(text)) && (match.index + match[0].length < anchor.character));
+        while ((match = regexRegex.exec(text)) && (match.index + match[0].length < anchor.character));
         if (match && match.index <= anchor.character) {
-            const regEx = createRegEx(match[1], match[2]);
-            if (regEx) {
+            const regex = createRegex(match[1], match[2]);
+            if (regex) {
                 return {
                     document: editor.document,
-                    regEx: regEx,
+                    regex: regex,
                     range: new vscode.Range(anchor.line, match.index, anchor.line, match.index + match[0].length)
                 };
             }
         }
     }
 
-    function createRegEx(pattern: string, flags: string) {
+    function createRegex(pattern: string, flags: string) {
             try {
                 return new RegExp(pattern, flags);
             } catch (e) {
@@ -169,12 +169,12 @@ deserunt mollit anim id est laborum.
             }
     }
 
-    function findMatches(regExMatch: RegExMatch, document: vscode.TextDocument) {
+    function findMatches(regexMatch: RegexMatch, document: vscode.TextDocument) {
         const text = document.getText();
         const matches: Match[] = [];
-        const regEx = regExMatch.regEx;
+        const regex = regexMatch.regex;
         let match: RegExpExecArray;
-        while ((regEx.global || !matches.length) && (match = regEx.exec(text))) {
+        while ((regex.global || !matches.length) && (match = regex.exec(text))) {
             matches.push({
                 range: new vscode.Range(document.positionAt(match.index), document.positionAt(match.index + match[0].length))
             });
